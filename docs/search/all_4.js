a43e9ef125210b077859112bd853ef72098f9885
Derek Stratton <derekdstratton@gmail.com>
2018-10-13 23:56:30 -0700

    Adding doxygen docs

---
+++ docs/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['poplevel',['popLevel',['../class_symbol_table.docs#a2b7d90f3fa8a1d79f2cbeef555f1375f',1,'SymbolTable']]],
+  ['pushlevel',['pushLevel',['../class_symbol_table.docs#a4ae4c247e136cd9d13224ba952fccb88',1,'SymbolTable']]]
+];
---
+++ docs/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "binops",
+  1: "ns",
+  2: "ips",
+  3: "o",
+  4: "b"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "related",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Friends",
+  4: "Pages"
+};
+
