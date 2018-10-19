51e56bd5354ed8b7ba9234781cf33d166decc279
Derek Stratton <derekdstratton@gmail.com>
2018-10-19 00:38:02 -0700

    Fixes

--- SymbolTableDriver.cpp
+++ SymbolTableDriver.cpp
@@ -12,17 +12,17 @@ int main() {
         cout << "What would you like to do now (Type 0 to view options, type -1 to exit)?" << endl;
         cin >> num;
         switch (num) {
-            case -1:
+            case -1:{
                 num = -1;
-                break;
-            case 0:
+                break;}
+            case 0: {
                 cout << "Option 1: Insert into Symbol Table." << endl;
                 cout << "Option 2: Search Symbol Table." << endl;
                 cout << "Option 3: Push Level onto Symbol Table." << endl;
                 cout << "Option 4: Pop Level off Symbol Table." << endl;
                 cout << "Option 5: Set mode of Symbol Table (insert/lookup)." << endl;
                 cout << "Option 6: Return current mode of Symbol Table." << endl;
-                break;
+                break;}
             case 1: {
                 cout << "Type an identifier to insert (A default node will be generated):" << endl;
                 string a;
@@ -33,10 +33,10 @@ int main() {
                 if (!insertSuccess) {
                     cout << "Element already exists at top scope. Failed to insert." << endl;
                 }
-                if (!notShadowing) {
+                else if (!notShadowing) {
                     cout << "Element successfully inserted, but is shadowing a lower level variable." << endl;
                 }
-                if (notShadowing) {
+                else if (notShadowing) {
                     cout << "Successfully inserted! " << endl;
                 }
                 break; }
---
+++ tests/test_toggle_production_outputs.c
@@ -0,0 +1,8 @@
+int main() {
+    !!P
+    int x = 3;
+    x = 4;
+    !!P
+    x = 6;
+    return 0;
+}
