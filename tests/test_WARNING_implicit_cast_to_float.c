e97fdae81060a3dd0d72fc765aeea64b42a77ba7
Derek Stratton <derekdstratton@gmail.com>
2018-11-08 22:54:49 -0800

    Add more test cases

--- tests/test_WARNING_implicit_cast_to_float.c
+++ tests/test_WARNING_implicit_cast_to_float.c
@@ -1,4 +1,5 @@
 int main() {
     float x;
     x = 1 + 2.0;
+    return 0;
 }
---
+++ tests/test_for_loop.c
@@ -0,0 +1,10 @@
+int main() {
+    int i;
+    int k;
+    i = 0;
+    k = 0;
+    for (i = 0; i < 5; i++) {
+        k += 2;
+    }
+    return 0;
+}
