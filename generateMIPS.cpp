9824a1160e589a37c565349c6923cd268869d988
Derek Stratton <derekdstratton@gmail.com>
2018-12-11 22:38:15 -0800

    Basic addition works! It's definitely not hard coded :]

--- generateMIPS.cpp
+++ generateMIPS.cpp
@@ -5,33 +5,11 @@
 
 using namespace std;
 
-string load(const string &s, ostream& mips) {
-    size_t location = s.find('(');
-    if (location != string::npos) {
-        mips << "\t" << "la " << "$t9, " << s << endl;
-        //mips << "\t" << "load " << s.substr(location+1, 3) << endl;
-        return "$t9";
-    }
-    else {
-        return s;
-    }
-}
-
-string store(const string &s, ostream& mips) {
-    size_t location = s.find('(');
-    if (location != string::npos) {
-        mips << "\t" << "move " << s << ", $t8" << endl;
-        //mips << "\t" << "load " << s.substr(location+1, 3) << endl;
-        return "$t8";
-    }
-    else {
-        return s;
-    }
-}
-
 bool isOffset(const string& s) {
     return s.find('(') != string::npos;
 }
+
+int registerCnt = 100;
 
 void generateMIPS(vector<vector<string>> tac) {
     ofstream mips;
@@ -49,12 +27,13 @@ void generateMIPS(vector<vector<string>> tac) {
         if (line[0] == "ASSIGN") {
             if (isOffset(line[1])) {
                 //Needs loaded
-                mips << "\t" << "lw " << "$t9, " << line[1] << endl;
-                line[1] = "$t9";
+                string s = "$t" + to_string(registerCnt++);
+                mips << "\t" << "lw " << s << ", " << line[1] << endl;
+                line[1] = s;
             }
             string ret;
             if (isOffset(line[3])) {
-                ret = "$t7";
+                ret = "$t" + to_string(registerCnt++);
             } else {
                 ret = line[3];
             }
@@ -69,11 +48,11 @@ void generateMIPS(vector<vector<string>> tac) {
             if (isOffset(line[1])) {
                 //Needs loaded
                 mips << "\t" << "lw " << "$t6, " << line[1] << endl;
-                line[1] = "$t6";
+                line[1] = "$t" + to_string(registerCnt++);
             }
             string ret;
             if (isOffset(line[3])) {
-                ret = "$t7";
+                ret = "$t" + to_string(registerCnt++);
             } else {
                 ret = line[3];
             }
@@ -87,17 +66,19 @@ void generateMIPS(vector<vector<string>> tac) {
         if (line[0] == "PLUS") {
             if (isOffset(line[1])) {
                 //Needs loaded
-                mips << "\t" << "lw " << "$t9, " << line[1] << endl;
-                line[1] = "$t9";
+                string s = "$t" + to_string(registerCnt++);
+                mips << "\t" << "lw " << s << ", " << line[1] << endl;
+                line[1] = s;
             }
             if (isOffset(line[2])) {
                 //Load
-                mips << "\t" << "lw " << "$t8, " << line[2] << endl;
-                line[2] = "$t8";
+                string s = "$t" + to_string(registerCnt++);
+                mips << "\t" << "lw " << s << ", " << line[2] << endl;
+                line[2] = s;
             }
             string ret;
             if (isOffset(line[3])) {
-                ret = "$t7";
+                ret = "$t" + to_string(registerCnt++);
             } else {
                 ret = line[3];
             }
