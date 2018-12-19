3067d185579a1db826017509f977c188fcc67ca2
Chris <chriskoh12@gmail.com>
2018-12-19 07:23:29 -0800

    Added even more final demo cases

---
+++ tests/Final_tests/test_ERROR_bad_parameters.c
@@ -0,0 +1,8 @@
+int testFunc(int a, int b){
+    return a + b;
+}
+
+int main(){
+    testFunc(3, 4, 5);
+    return 0;
+}
---
+++ tests/Final_tests/test_ERROR_redeclare_function.c
@@ -0,0 +1,12 @@
+int testFunc(int a, int b){
+    return a + b;
+}
+
+int testFunc(int c, int d)
+{
+    return c - d;
+}
+
+int main(){
+    return 0;
+}
